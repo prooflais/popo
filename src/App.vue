ef3018fd3fe446614dc3007e685c9d3872288455
prooflais <pufferproo@gmail.com>
2024-09-06 01:03:00 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,203 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const name = ref('');
+    const lastName = ref('');
+    const year = ref('');
+    const phone = ref('');
+    const address = ref('');
+    const email = ref('');
+    const datos = ref<any[]>([]);
+    const errorName = ref('');
+    const errorLastName = ref('');
+    const errorYear = ref('');
+    const errorPhone = ref('');
+    const errorAddress = ref('');
+    const errorEmail = ref('');
+
+    const reset = () => {
+      name.value = '';
+      lastName.value = '';
+      year.value = '';
+      phone.value = '';
+      address.value = '';
+      email.value = '';
+
+      errorName.value = '';
+      errorLastName.value = '';
+      errorYear.value = '';
+      errorPhone.value = '';
+      errorAddress.value = '';
+      errorEmail.value = '';
+    };
+
+    const validar = () => {
+      let isValid = true;
+
+      if (!name.value) {
+        errorName.value = 'Name is required';
+        isValid = false;
+      } else {
+        errorName.value = '';
+      }
+
+      if (!lastName.value) {
+        errorLastName.value = 'Last Name is required';
+        isValid = false;
+      } else {
+        errorLastName.value = '';
+      }
+
+      if (!year.value) {
+        errorYear.value = 'Year of Birth is required';
+        isValid = false;
+      } else {
+        errorYear.value = '';
+      }
+
+      if (!phone.value) {
+        errorPhone.value = 'Phone is required';
+        isValid = false;
+      } else {
+        errorPhone.value = '';
+      }
+
+      if (!address.value) {
+        errorAddress.value = 'Address is required';
+        isValid = false;
+      } else {
+        errorAddress.value = '';
+      }
+
+      if (!email.value) {
+        errorEmail.value = 'Email is required';
+        isValid = false;
+      } else {
+        errorEmail.value = '';
+      }
+
+      return isValid;
+    };
+
+    // agregar los datos atabla
+    const enviar = () => {
+      if (validar()) {
+        
+        datos.value.push({
+          name: name.value,
+          lastName: lastName.value,
+          year: year.value,
+          phone: phone.value,
+          address: address.value,
+          email: email.value
+        });
+        reset();
+      }
+    };
+
+    return {
+      name,
+      lastName,
+      year,
+      phone,
+      address,
+      email,
+      datos,
+      errorName,
+      errorLastName,
+      errorYear,
+      errorPhone,
+      errorAddress,
+      errorEmail,
+      reset,
+      enviar
+    };
+  }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="registro">
+    <h4>Registro</h4>
+    <hr>
+    <div>
+      <label for="name" class="textos">Nombre:</label>
+      <input v-model="name" type="text" id="name" @blur="validateName" />
+      <span v-if="errorName" style="color: red;">{{ errorName }}</span>
+    </div>
+
+    <div>
+      <label for="lastName" class="textos">Apellido:</label>
+      <input v-model="lastName" type="text" id="lastName" />
+      <span v-if="errorLastName" style="color: red;">{{ errorLastName }}</span>
+    </div>
+
+    <div>
+      <label for="year" class="textos">Año nacimiento:</label>
+      <input v-model="year" type="number" id="year" />
+      <span v-if="errorYear" style="color: red;">{{ errorYear }}</span>
+    </div>
+
+    <div>
+      <label for="phone" class="textos">Telefono:</label>
+      <input v-model="phone" type="tel" id="phone" />
+      <span v-if="errorPhone" style="color: red;">{{ errorPhone }}</span>
+    </div>
+
+    <div>
+      <label for="address" class="textos">Contrasena:</label>
+      <input v-model="address" type="text" id="address" />
+      <span v-if="errorAddress" style="color: red;">{{ errorAddress }}</span>
+    </div>
+
+    <div>
+      <label for="email" class="textos">Email:</label>
+      <input v-model="email" type="email" id="email" />
+      <span v-if="errorEmail" style="color: red;">{{ errorEmail }}</span>
+    </div>
+    <hr>
+    
+      <button @click="enviar">Registrar</button>
+      <button @click="reset">Limpiar</button>
+
+    <table v-if="datos.length" border="1" style="margin-top: 20px;">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Año nacimiento</th>
+          <th>Telefono</th>
+          <th>Contraseña</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in datos" :key="index">
+          <td>{{ data.name }}</td>
+          <td>{{ data.lastName }}</td>
+          <td>{{ data.year }}</td>
+          <td>{{ data.phone }}</td>
+          <td>{{ data.address }}</td>
+          <td>{{ data.email }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.registro{
+  border: 1px solid;
+  padding-inline: 30px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.textos{
+  display: block;
+  width: 100%;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+span{
+  display: block;
+  width: 100%;
 }
 </style>
